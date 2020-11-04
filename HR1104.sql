5753a2a7bda383cb761cd7400f864ca90ab6ccde
Yejin <goyejin25@gmail.com>
2020-11-04 16:44:37 +0900

    oracle source

--- HR1104.sql
+++ HR1104.sql
@@ -1,4 +1,20 @@
-SELECT
-    *
-FROM
-tab;
+-- HR 계정이 가지고 있는 모든 테이블 보기
+select * from tab;
+
+desc employees;
+
+-- employees 테이블 전체 내용 조회
+SELECT * FROM employees;
+
+-- employees 테이블의 first_name, last_name, job_id 만 조회
+SELECT first_name, last_name, job_id FROM employees;
+
+-- 사원번호가 176인 사람의 LAST_NAME과 부서번호 조회
+SELECT last_name, deptid WHERE employee_id = 176;
+
+-- 연봉이 12000이상 (>=) 되는 직원들의 LAST_NAME 및 연봉조회
+SELECT last_name, salary FROM employees WHERE salary >= 12000;
+
+-- 연봉이 5000에서 12000의 범위 이외의 사람들의 LAST_NAME 및 연봉 조회
+SELECT last_name, salary FROM employees WHERE salary < 5000 OR
+        salary > 12000;
--- scott1104.sql
+++ scott1104.sql
@@ -1,1 +1,146 @@
+-- SCOTT 계정이 가지고 있는 모든 테이블 보기
 select * from tab;
+
+-- emp 테이블의 구조
+DESC emp;
+
+
+-- emp 테이블의 모든 내용 조회
+SELECT * FROM emp;
+
+-- emp 테이블의 특정 필드만 조회
+SELECT ename, job, sal FROM emp;
+
+
+
+-- 중복 레코드 제거한 후 조회 : DISTINCT
+SELECT DISTINCT deptno FROM emp;
+
+-- alais(별칭) : AS
+SELECT empno AS 사원번호 FROM emp;
+
+SELECT ename, sal, sal*12+comm AS 연봉, comm FROM emp;
+
+SELECT ename as 사원이름, job AS "직 책" from emp;
+
+-- ORDER BY : 정렬하여 조회
+
+-- 하나의 열로 기준을 주어 오름차순(ASC) 조회
+SELECT ename,sal FROM emp ORDER BY sal;
+
+-- 하나의 열로 기준을 주어 내림차순 조회
+SELECT ename,sal FROM emp ORDER BY sal DESC;
+
+-- 전체 데이터를 조회(사원번호 오름차순)
+SELECT * FROM emp ORDER BY empno;
+
+-- 부서번호의 오름차순이고, 부서번호가 동일하다면 급여의 내림차순
+SELECT * FROM emp ORDER BY deptno ASC, sal DESC;
+
+-- 출력되는 열의 별칭을 다음과 같이 지정한다.
+-- EMPNO : EMPLOYEE_NO
+-- ENAME : EMPLOYEE_NAME
+-- MGR : MANAGER
+-- SAL : SALARY
+-- COMM : COMMISSION
+-- DEPTNO : DEPARTMENT_NO
+
+-- 부서번호를 기준으로 내림차순으로 정렬하되 부서 번호가 같다면 사원 이름을 기준으로 오름차순 정렬한다.
+
+SELECT empno AS EMPLOYEE_NO, ename AS EMPLOYEE_NAME, job, mgr AS MANAGER,
+       hiredate, sal AS SALARY, comm AS COMMISSION, deptno AS DEPARTMENT_NO
+FROM emp ORDER BY deptno DESC, ename ASC;
+
+-- WHERE : 조회를 할 때 기준 주기
+
+-- 부서번호가 30번인 사원 조회
+SELECT * FROM emp WHERE deptno = 30;
+
+-- 사원번호가 7782인 사원 조회
+SELECT * FROM emp WHERE empno=7782;
+
+-- 부서번호가 30이고, 사원직책이 SALESMAN 정보 조회
+SELECT * FROM emp WHERE deptno=30 AND job='SALESMAN';
+
+-- 사원번호가 7499이고, 부서번호가 30인 정보 조회
+SELECT * FROM emp WHERE empno=7499 AND deptno=30;
+
+-- 부서번호가 30이거나, 사원직책이 CLERK 정보 조회
+SELECT * FROM emp WHERE deptno=30 OR job='CLERK';
+
+-- 부서번호가 20이거나, 사원직책이 SALESMAN 정보 조회
+SELECT * FROM emp WHERE deptno=20 OR job='SALESMAN';
+
+-- 사원번호가 176인 사람의 LAST_NAME과 부서번호 조회
+SELECT LAST_NAME, deptno FROM emp W-- SCOTT 계정이 가지고 있는 모든 테이블 보기
+select * from tab;
+
+-- emp 테이블의 구조
+DESC emp;
+
+
+-- emp 테이블의 모든 내용 조회
+SELECT * FROM emp;
+
+-- emp 테이블의 특정 필드만 조회
+SELECT ename, job, sal FROM emp;
+
+
+
+-- 중복 레코드 제거한 후 조회 : DISTINCT
+SELECT DISTINCT deptno FROM emp;
+
+-- alais(별칭) : AS
+SELECT empno AS 사원번호 FROM emp;
+
+SELECT ename, sal, sal*12+comm AS 연봉, comm FROM emp;
+
+SELECT ename as 사원이름, job AS "직 책" from emp;
+
+-- ORDER BY : 정렬하여 조회
+
+-- 하나의 열로 기준을 주어 오름차순(ASC) 조회
+SELECT ename,sal FROM emp ORDER BY sal;
+
+-- 하나의 열로 기준을 주어 내림차순 조회
+SELECT ename,sal FROM emp ORDER BY sal DESC;
+
+-- 전체 데이터를 조회(사원번호 오름차순)
+SELECT * FROM emp ORDER BY empno;
+
+-- 부서번호의 오름차순이고, 부서번호가 동일하다면 급여의 내림차순
+SELECT * FROM emp ORDER BY deptno ASC, sal DESC;
+
+-- 출력되는 열의 별칭을 다음과 같이 지정한다.
+-- EMPNO : EMPLOYEE_NO
+-- ENAME : EMPLOYEE_NAME
+-- MGR : MANAGER
+-- SAL : SALARY
+-- COMM : COMMISSION
+-- DEPTNO : DEPARTMENT_NO
+
+-- 부서번호를 기준으로 내림차순으로 정렬하되 부서 번호가 같다면 사원 이름을 기준으로 오름차순 정렬한다.
+
+SELECT empno AS EMPLOYEE_NO, ename AS EMPLOYEE_NAME, job, mgr AS MANAGER,
+       hiredate, sal AS SALARY, comm AS COMMISSION, deptno AS DEPARTMENT_NO
+FROM emp ORDER BY deptno DESC, ename ASC;
+
+-- WHERE : 조회를 할 때 기준 주기
+
+-- 부서번호가 30번인 사원 조회
+SELECT * FROM emp WHERE deptno = 30;
+
+-- 사원번호가 7782인 사원 조회
+SELECT * FROM emp WHERE empno=7782;
+
+-- 부서번호가 30이고, 사원직책이 SALESMAN 정보 조회
+SELECT * FROM emp WHERE deptno=30 AND job='SALESMAN';
+
+-- 사원번호가 7499이고, 부서번호가 30인 정보 조회
+SELECT * FROM emp WHERE empno=7499 AND deptno=30;
+
+-- 부서번호가 30이거나, 사원직책이 CLERK 정보 조회
+SELECT * FROM emp WHERE deptno=30 OR job='CLERK';
+
+-- 부서번호가 20이거나, 사원직책이 SALESMAN 정보 조회
+SELECT * FROM emp WHERE deptno=20 OR job='SALESMAN';
